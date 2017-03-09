6c89a36f149fae56ddb31ddaa52f63bb88c53407
deathsythe47 <deathsythe47@gmail.com>
2017-03-09 11:12:08 -0600

    change cl_allowEnterCompletion default to 1

--- codemp/client/cl_main.cpp
+++ codemp/client/cl_main.cpp
@@ -2748,7 +2748,7 @@ void CL_Init( void ) {
 	cl_framerate	= Cvar_Get ("cl_framerate", "0", CVAR_TEMP);
 	cl_allowDownload = Cvar_Get ("cl_allowDownload", "0", CVAR_ARCHIVE, "Allow downloading custom paks from server");
 	cl_allowAltEnter = Cvar_Get ("cl_allowAltEnter", "1", CVAR_ARCHIVE, "Enables use of ALT+ENTER keyboard combo to toggle fullscreen" );
-	cl_allowEnterCompletion = Cvar_Get("cl_allowEnterCompletion", "0", CVAR_ARCHIVE, "Enables autocomplete when pressing enter");
+	cl_allowEnterCompletion = Cvar_Get("cl_allowEnterCompletion", "1", CVAR_ARCHIVE, "Enables autocomplete when pressing enter");
 
 	cl_autolodscale = Cvar_Get( "cl_autolodscale", "1", CVAR_ARCHIVE );
 
