1b95500eff5afa9731acbc697894b857870348db
deathsythe47 <deathsythe47@gmail.com>
2017-03-31 11:05:50 -0500

    extend cl_maxpackets lower bound to 15

--- codemp/client/cl_input.cpp
+++ codemp/client/cl_input.cpp
@@ -1457,8 +1457,8 @@ qboolean CL_ReadyToSendPacket( void ) {
 	}
 
 	// check for exceeding cl_maxpackets
-	if ( cl_maxpackets->integer < 20 ) {
-		Cvar_Set( "cl_maxpackets", "20" );
+	if ( cl_maxpackets->integer < 15 ) {
+		Cvar_Set( "cl_maxpackets", "15" );
 	}
 	else if ( cl_maxpackets->integer > 1000 ) {
 		Cvar_Set( "cl_maxpackets", "1000" );
