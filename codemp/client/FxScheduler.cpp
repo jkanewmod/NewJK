65e91cede8946180b69febb1ca4e06aa994e9f07
deathsythe47 <deathsythe47@gmail.com>
2017-05-25 11:59:19 -0700

    derp

--- codemp/client/FxScheduler.cpp
+++ codemp/client/FxScheduler.cpp
@@ -404,11 +404,10 @@ static const size_t numPrimitiveTypes = ARRAY_LEN( primitiveTypes );
 // hack for known projectile filenames, we will force their life to 1 (concussion is intentionally omitted)
 static char projectileFilenames[][32] = {
 	"atst/shot", "atst/shot_red", "atst/side_alt_shot", "atst/side_main_shot", "blaster/npcshot", "blaster/shot",
-	"bowcaster/shot", "bryar/crackleShot", "bryar/npcshot", "bryar/shot", "demp2/projectile", "emplaced/shot",
-	"emplaced/shotnpc", "eweb/shot", "eweb/shotnpc", "flechette/alt_shot", "flechette/shot", "noghri_stick/shot",
-	"repeater/alt_projectile", "repeater/projectile", "rocket/shot", "ships/imp_blastershot", "ships/imp_torpshot",
-	"ships/mine", "ships/reb_blastershot", "ships/reb_torpshot", "ships/swoop_blastershot", "turret/hoth_shot",
-	"turret/shot", "turret/turb_shot", "tusken/shot",
+	"bowcaster/shot", "bryar/crackleShot", "bryar/npcshot", "demp2/projectile", "emplaced/shot", "emplaced/shotnpc",
+	"eweb/shot", "eweb/shotnpc", "flechette/alt_shot", "flechette/shot", "noghri_stick/shot", "repeater/alt_projectile",
+	"repeater/projectile", "rocket/shot", "ships/imp_blastershot", "ships/imp_torpshot", "ships/mine", "ships/reb_blastershot",
+	"ships/reb_torpshot", "ships/swoop_blastershot", "turret/hoth_shot", "turret/shot", "turret/turb_shot", "tusken/shot",
 };
 
 static int compareFilenames(const void *a, const void *b) {
